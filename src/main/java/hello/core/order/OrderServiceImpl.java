6378e14e018034e402f1209cce83c2dea80f03d0
Mina Park <kyminp@gmail.com>
2022-08-15 14:53:11 +0900

    add:관심사의 분리(appconfig를 통한 할당)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,28 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // DIP 위반: OrderServiceImpl은 인터페이스인 discountPolicy에만 의존하는 것처럼 보이지만 실은 구체 클래스인 FixDiscountPolicy에도 의존하고 있음
+    // OCP 위반: 변경발생으로 인해 클라이언트 코드 수정이 불가피
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    //생성자를 통한 주입
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
